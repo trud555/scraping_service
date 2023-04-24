95eb991ee5f180952856ba44199dd6e322da087b
Ruslan <trud555@ukr.net>
2023-04-24 18:09:34 +0300

    add home in scraping.view

--- scraping/views.py
+++ scraping/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Vacancy
 
-# Create your views here.
+def home(request):
+    qs = Vacancy.objects.all()
+    return  render(request, 'home.html', {'object_list': qs})
--- scraping_service/urls.py
+++ scraping_service/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import *
+from scraping.views import *
 
 urlpatterns = [
     path('adminok/', admin.site.urls),
